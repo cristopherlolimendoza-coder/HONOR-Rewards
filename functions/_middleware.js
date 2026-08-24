393d1d1f5177fd216ecf1468d0021012543eaf50
cristopherlolimendoza-coder <cristopherlolimendoza@gmail.com>
2026-08-24 17:25:45 -0500

    Fuerza corrección Aerolight v19

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,6 +3,6 @@ export async function onRequest(context){
   const type=response.headers.get('content-type')||'';
   if(!type.includes('text/html')) return response;
   return new HTMLRewriter()
-    .on('body',{element(el){el.append('<script src="/image-fix.js?v=18"></script>',{html:true});}})
+    .on('body',{element(el){el.append('<script src="/image-fix.js?v=19"></script>',{html:true});}})
     .transform(response);
 }
