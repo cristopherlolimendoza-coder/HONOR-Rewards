2def70225e4c493b3b7673ce334f10c3076071c3
cristopherlolimendoza-coder <cristopherlolimendoza@gmail.com>
2026-08-25 12:40:49 -0500

    Publica imagen Aerolight validada v23

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,6 +3,6 @@ export async function onRequest(context){
   const type=response.headers.get('content-type')||'';
   if(!type.includes('text/html')) return response;
   return new HTMLRewriter()
-    .on('body',{element(el){el.append('<script src="/image-fix.js?v=22"></script>',{html:true});}})
+    .on('body',{element(el){el.append('<script src="/image-fix.js?v=23"></script>',{html:true});}})
     .transform(response);
 }
